b072cb014d5563fac4923304085297d9f45bb140
lachlan.dowd <lachlan.dowd@gmail.com>
2015-08-28 09:17:32 +1000

    Additional files to support CM.

---
+++ LedShowEditor/AppBootstrapper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using Caliburn.Micro;
+
+namespace LedShowEditor
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        protected CompositionContainer Container;
+
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        /// <summary>
+        /// Add various services that we require to the container
+        /// </summary>
+        protected override void Configure()
+        {
+            Container = new CompositionContainer(new AggregateCatalog(
+                AssemblySource.Instance.Select(x => new AssemblyCatalog(x)).OfType<ComposablePartCatalog>()));
+
+            var batch = new CompositionBatch();
+
+            var window = new WindowManager();
+            batch.AddExportedValue<IWindowManager>(window);
+
+            var eventAggregator = new EventAggregator();
+            batch.AddExportedValue<IEventAggregator>(eventAggregator);
+
+            batch.AddExportedValue(Container);
+
+            Container.Compose(batch);
+        }
+
+        protected override object GetInstance(Type serviceType, string key)
+        {
+            string contract = string.IsNullOrEmpty(key) ? AttributedModelServices.GetContractName(serviceType) : key;
+            var exports = Container.GetExportedValues<object>(contract);
+
+            if (exports.Count() > 0)
+                return exports.First();
+
+            throw new Exception(string.Format("Could not locate any instances of contract {0}.", contract));
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type serviceType)
+        {
+            return Container.GetExportedValues<object>(AttributedModelServices.GetContractName(serviceType));
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            Container.SatisfyImportsOnce(instance);
+        }
+
+        protected override IEnumerable<Assembly> SelectAssemblies()
+        {
+            return new[]
+            {
+                Assembly.GetExecutingAssembly()
+            };
+        }
+
+        /// <summary>
+        /// Set the View to use Shell on startup
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">startup event args</param>
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<IShell>();
+        }
+    }
+}
---
+++ LedShowEditor/ShellViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.Composition;
+using Caliburn.Micro;
+
+namespace LedShowEditor
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Export(typeof(IShell))]
+    public class ShellViewModel : Conductor<IScreen>.Collection.AllActive, IShell
+    {
+
+        private readonly IEventAggregator _eventAggregator;
+
+
+        /// <summary>
+        /// Constructor for the ShellViewModel. Main container for Client application elements.
+        /// Imports required UI elements via DI.
+        /// </summary>
+        /// <param name="eventAggregator"></param>    
+        [ImportingConstructor]
+        public ShellViewModel(
+            IEventAggregator eventAggregator
+            )
+        {
+            _eventAggregator = eventAggregator;
+
+        }
+
+
+
+        protected override void OnViewLoaded(object view)
+        {
+
+            base.OnViewLoaded(view);
+
+            _eventAggregator.Subscribe(this);
+
+        }
+
+
+
+
+
+    }
+}
